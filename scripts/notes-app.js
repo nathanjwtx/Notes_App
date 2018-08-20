f4e9e539d897dfa0f060e7738a43a74af95f8fc3
Nathan Waterman <nathanjwuk@gmail.com>
2018-08-20 08:03:05 -0500

    Initial code push

---
+++ scripts/notes-app.js
@@ -0,0 +1,45 @@
+// const notes = [{
+//   title: 'note1',
+//   body: 'I need a holiday'
+// }, {
+//   title: 'note3',
+//   body: 'Looking forward to my next hammock outing'
+// }, {
+//   title: 'note2',
+//   body: 'Looks like rain'
+// }]
+
+// DOM - Document Object Model
+
+// document.querySelector('#delete-note').addEventListener('click', function () {
+//   document.querySelectorAll('.note-class').forEach(function (note) {
+//     console.log('remove note')
+//   })
+// })
+
+const notes = getSavedNotes()
+
+const filters = {
+  searchText: ''
+}
+
+renderNotes(notes, filters)
+
+document.querySelector('#search-text').addEventListener('input', function (e) {
+  filters.searchText = e.target.value
+  renderNotes(notes, filters)
+})
+
+document.querySelector('#filterBy').addEventListener('change', function (e) {
+  console.log(e.target.value)
+})
+
+document.querySelector('#add-note').addEventListener('click', function () {
+  notes.push({
+    id: uuidv4(),
+    title: '',
+    body: ''
+  })
+  saveNotes(notes)
+  renderNotes(notes, filters)
+})
---
+++ scripts/notes-functions.js
@@ -0,0 +1,65 @@
+// read existing notes from localStorage
+const getSavedNotes = function () {
+    const notesJSON = localStorage.getItem('notes')
+
+    if (notesJSON != null) {
+        return JSON.parse(notesJSON)
+    } else {
+        return []
+    }
+}
+
+const saveNotes = function (notes) {
+    localStorage.setItem('notes', JSON.stringify(notes))
+}
+
+// generate DOM structure for a note
+const generateNoteDOM = function (note) {
+    const noteElement = document.createElement('div')
+    const textElement = document.createElement('p')
+    const delButton = document.createElement('button')
+
+    // setup remove note button
+    delButton.textContent = 'X'
+    noteElement.appendChild(delButton)
+    delButton.addEventListener('click', function () {
+        removeNote(note.id)
+        saveNotes(notes)
+        renderNotes(notes, filters)
+    })
+
+    // setup the note title text
+    if (note.title.length > 0) {
+        textElement.textContent = note.title
+    } else {
+        textElement.textContent = 'Unnamed note'
+    }
+    noteElement.appendChild(textElement)
+    
+
+    return noteElement
+}
+
+// remove a note
+const removeNote = function (id) {
+    const noteIndex = notes.findIndex(function (note) {
+        return note.id = id
+    })
+    if (noteIndex > -1) {
+        notes.splice(noteIndex, 1)
+    }
+}
+
+// render application notes
+const renderNotes = function (notes, filters) {
+    const filteredNotes = notes.filter(function (note) {
+        return note.body.toLowerCase().includes(filters.searchText.toLowerCase())
+    })
+    // clear the <div> element to only show the note one time
+    document.querySelector('#notes').innerHTML = ''
+
+    filteredNotes.forEach(function (note) {
+        const noteElement = generateNoteDOM(note)
+        document.querySelector('#notes').appendChild(noteElement)
+    })
+}
